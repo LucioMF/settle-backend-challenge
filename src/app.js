1157d3df2af4eade192e00f673db8dd477d1f728
LucioMF <luciof10.17@gmail.com>
2022-03-23 03:36:28 -0300

    basic server config for HapiJS

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const Hapi = require('@Hapi/hapi');
+
+const server = Hapi.server({
+  port: 8080,
+  host: 'localhost',
+  app: {}
+});
+
+const startServer = async () => {
+  try {
+    // await server.register(routes);
+    await server.start();
+    console.log(`Server running on: ${server.info.uri}`);
+  } catch (error) {
+    console.log(`Error trying to run HapiJs server: ${error}`);
+  }
+};
+
+startServer();
