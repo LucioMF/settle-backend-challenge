0984e31652623d9a09cdd9dac27d6249148a7d96
LucioMF <luciof10.17@gmail.com>
2022-03-29 20:38:44 -0300

    remove unnecesary file

--- src/database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = requiree('mongoose');
-
-mongoose.connect('monodb://localhost:3001/settle_challenge', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => console.log('Database connection established'))
-.catch((e) => console.error(`Database connection error: ${e}`));
